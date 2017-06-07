8eadf3c7949d8231f276dfd0f07526bca30bef41
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2017-06-07 16:49:31 -0400

    grab name

--- A/B/C/index.js
+++ A/B/C/index.js
@@ -1,3 +1,13 @@
 #!/usr/bin/env node
 
+console.error('args', process.argv)
+const program = process.argv[2]
+if (!program) {
+  console.error('bin-up: finds bin link in node_modules/.bin folders')
+  console.error('up to the Git root folder')
+  console.error('usage: $(bin-up <name>)')
+  console.error('for example:')
+  console.error('$(bin-up mocha) --verbose test/*.js')
+  process.exit(1)
+}
 console.log('../../../node_modules/.bin/figlet')
